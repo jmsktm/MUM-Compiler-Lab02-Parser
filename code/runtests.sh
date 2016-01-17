c772f136d323ad16e11cb06d980342c7d2c2f361
James Singh <james@icloudtech.com>
2016-01-17 12:53:22 -0800

    Formatting

--- code/runtests.sh
+++ code/runtests.sh
@@ -11,61 +11,61 @@ javac ParserDriver.java
 
 echo "########## RUNNING TESTS #########"
 
-echo "java ParserDriver tests/in01.cp > output.txt" > output.txt
+echo "java ParserDriver tests/in01.cp" > output.txt
 java ParserDriver tests/in01.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in02.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in02.cp" >> output.txt
 java ParserDriver tests/in02.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in03.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in03.cp" >> output.txt
 java ParserDriver tests/in03.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in04.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in04.cp" >> output.txt
 java ParserDriver tests/in04.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in05.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in05.cp" >> output.txt
 java ParserDriver tests/in05.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in06.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in06.cp" >> output.txt
 java ParserDriver tests/in06.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in07.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in07.cp" >> output.txt
 java ParserDriver tests/in07.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in08.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in08.cp" >> output.txt
 java ParserDriver tests/in08.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in09.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in09.cp" >> output.txt
 java ParserDriver tests/in09.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
 
-echo "java ParserDriver tests/in10.cp >> output.txt" >> output.txt
+echo "java ParserDriver tests/in10.cp" >> output.txt
 java ParserDriver tests/in10.cp >> output.txt
 echo "" >> output.txt
 echo "" >> output.txt
